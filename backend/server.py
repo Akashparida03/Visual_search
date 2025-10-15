0d4aff1d634ee3c6f276efd66dcbd53fd293bfef
akashparida03 <157849226+Akashparida03@users.noreply.github.com>
2025-10-15 12:57:56 +0530

    Update server.py
    port change

--- backend/server.py
+++ backend/server.py
@@ -1,7 +1,10 @@
 import uvicorn
 import os # Import the os module
+from dotenv import load_dotenv
+
+load_dotenv()
 
 if __name__ == "__main__":
     # Get the port from the environment variable Render provides
-    port = int(os.environ.get("PORT", 8000)) 
+    port = os.getenv("PORT", 8000)) 
     uvicorn.run("main:app", host="0.0.0.0", port=port)
